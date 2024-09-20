7daa6ce7cc96833c18f4d92642eb835ff5c1fa1f
yunhao chen <w0847446@myscc.ca>
2024-09-20 13:22:15 -0400

    creating component and link it to app.component page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ContentCardComponent} from "./content-card/content-card.component";
 
+interface IContent{
+  id:number,
+  imageUrl:string,
+  body:string,
+  type:string
+}
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContentCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'My-First-Project';
+  individualContentItem: IContent;
+  constructor(){
+    this.individualContentItem = {
+              id: 1,
+              imageUrl: 'https://angular.io/assets/images/logos/angular/angular.png',
+              body: 'This is the body of the content....',
+              type: 'news'
+    };
+  }
 }
