d0e782211fbecc814fac9e2fdede079b8d89cf6e
yunhao chen <w0847446@myscc.ca>
2024-10-29 09:55:09 -0400

    learning reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {ContentItemComponent} from "./content-item/content-item.component";
-import {FormsModule} from "@angular/forms";
-import {NgIf} from "@angular/common";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgForOf, NgIf} from "@angular/common";
+
 
 interface IContent{
   id:number,
@@ -13,13 +14,15 @@ interface IContent{
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContentItemComponent, RouterLink, RouterLinkActive, FormsModule, NgIf],
+  imports: [RouterOutlet, ContentItemComponent, RouterLink, RouterLinkActive, FormsModule, NgIf, ReactiveFormsModule, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title:string='';
   individualContentItem: IContent;
+  customerForm!: FormGroup;
+  countryList = [{countryName:'USA'},{countryName:'CN'},{countryName:'Canada'},{countryName:'India'}]
   constructor(){
     this.individualContentItem = {
               id: 1,
@@ -36,6 +39,47 @@ export class AppComponent {
   onSubmit(form:any){
     if(form.valid){
       console.log('Form submitted successfully', this.user)
+    }else{
+      console.log('Form is invalid')
+    }
+  }
+
+  ngOnInit(): void {
+    this.customerForm = new FormGroup({
+      'firstName': new FormControl(null, Validators.required),
+      "lastName" : new FormControl(null, Validators.required),
+      'email' : new FormControl(null, [Validators.required, Validators.email]),
+      'gender' : new FormControl('female'),
+      'isMarried': new FormControl(null, Validators.required),
+      'address': new FormGroup({
+        'country': new FormControl(null, Validators.required),
+        'city' : new FormControl(null, Validators.required),
+        'street' : new FormControl(null, Validators.required),
+        'pincode' : new FormControl(null,Validators.required),
+      })
+    })
+    this.customerForm.setValue({
+      firstName:'yunhao',
+      lastName: 'chen',
+      email:'w0847446@myscc.ca',
+      gender:'male',
+      isMarried:true,
+      address: {
+        country: 'Canada',
+        city:'Windsor',
+        street:'kennedy',
+        pincode:'234234'
+      }
+    })
+
+    this.customerForm.patchValue({
+      firstName:'Y',
+      gender:'female',
+    })
+  }
+  onSubmitActive(){
+    if(true){
+      console.log('Form submitted successfully', this.customerForm)
     }else{
       console.log('Form is invalid')
     }
